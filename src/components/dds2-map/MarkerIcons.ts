68e8013cd2a761501ef6563ef6af6be19298fc2c
absmaa21 <113515770+absmaa21@users.noreply.github.com>
2025-03-18 22:30:11 +0100

    Fix popup anchor position for marker icons
    Updated the popupAnchor property for various marker icons to adjust their positioning. Changed values from `[0, iconSize[1]]` to `[0, -iconSize[1]]` for better alignment.

--- src/components/dds2-map/MarkerIcons.ts
+++ src/components/dds2-map/MarkerIcons.ts
@@ -16,68 +16,68 @@ export const shopIcon = new L.Icon({
   iconUrl: shopImg,
   iconSize,
   iconAnchor: [iconSize[0] / 2, iconSize[1]],
-  popupAnchor: [0, iconSize[1]]
+  popupAnchor: [0, -iconSize[1]]
 })
 
 export const hideoutIcon = new L.Icon({
   iconUrl: hideoutImg,
   iconSize,
   iconAnchor: [iconSize[0] / 2, iconSize[1]],
-  popupAnchor: [0, iconSize[1]]
+  popupAnchor: [0, -iconSize[1]]
 })
 
 export const pawnshopIcon = new L.Icon({
   iconUrl: pawnshopImg,
   iconSize,
   iconAnchor: [iconSize[0] / 2, iconSize[1]],
-  popupAnchor: [0, iconSize[1]]
+  popupAnchor: [0, -iconSize[1]]
 })
 
 export const medicIcon = new L.Icon({
   iconUrl: medicImg,
   iconSize,
   iconAnchor: [iconSize[0] / 2, iconSize[1]],
-  popupAnchor: [0, iconSize[1]]
+  popupAnchor: [0, -iconSize[1]]
 })
 
 export const anchorIcon = new L.Icon({
   iconUrl: anchorImg,
   iconSize,
   iconAnchor: [iconSize[0] / 2, iconSize[1]],
-  popupAnchor: [0, iconSize[1]]
+  popupAnchor: [0, -iconSize[1]]
 })
 
 export const heartIcon = new L.Icon({
   iconUrl: heartImg,
   iconSize,
   iconAnchor: [iconSize[0] / 2, iconSize[1]],
-  popupAnchor: [0, iconSize[1]]
+  popupAnchor: [0, -iconSize[1]]
 })
 
 export const restaurantIcon = new L.Icon({
   iconUrl: restaurantImg,
   iconSize,
   iconAnchor: [iconSize[0] / 2, iconSize[1]],
-  popupAnchor: [0, iconSize[1]]
+  popupAnchor: [0, -iconSize[1]]
 })
 
 export const clothIcon = new L.Icon({
   iconUrl: clothImg,
   iconSize,
   iconAnchor: [iconSize[0] / 2, iconSize[1]],
-  popupAnchor: [0, iconSize[1]]
+  popupAnchor: [0, -iconSize[1]]
 })
 
 export const furnitureIcon = new L.Icon({
   iconUrl: furnitureImg,
   iconSize,
   iconAnchor: [iconSize[0] / 2, iconSize[1]],
-  popupAnchor: [0, iconSize[1]]
+  popupAnchor: [0, -iconSize[1]]
 })
 
 export const dealerIcon = new L.Icon({
   iconUrl: dealerImg,
   iconSize,
   iconAnchor: [iconSize[0] / 2, iconSize[1]],
-  popupAnchor: [0, iconSize[1]]
+  popupAnchor: [0, -iconSize[1]]
 })
